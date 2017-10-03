3e5fe5a190f1d9fd164d7cfdeb94368fca44a21a
Juan C. Gomez <prc3205@us.ad.hertz.com>
2017-10-03 09:11:56 -0400

    removed munits

--- src/main/java/com/hertz/cosmos/pojo/FlightData.java
+++ src/main/java/com/hertz/cosmos/pojo/FlightData.java
@@ -1,5 +1,14 @@
 package com.hertz.cosmos.pojo;
 
-public class FlightData {
+public class FlightData extends AbstractCosmosPojo {
 
+	public FlightData(String databaseName, String collectionId) {
+		super(databaseName, collectionId);
+	}
+
+	@Override
+	public String getAllQuery() {			
+		return "SELECT TOP 50 * FROM " + getCollectionId();
+	}	
+	
 }
---
+++ src/test/java/com/hertz/cosmos/FlightDataConnectivityTest.java
@@ -0,0 +1,75 @@
+package com.hertz.cosmos;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.mule.util.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.hertz.cosmos.utils.CosmosUtils;
+import com.microsoft.azure.documentdb.Document;
+import com.microsoft.azure.documentdb.DocumentClientException;
+
+import org.springframework.test.context.junit4.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:beans-config.xml", 
+        "classpath:test-beans.xml"})
+public class FlightDataConnectivityTest {
+	
+	Logger logger = LogManager.getLogger(this.getClass());
+	
+	@Autowired
+	CosmosUtils cosmosFlightDataUtil = null;
+		
+	@Before
+	public void startUp() throws DocumentClientException, IOException {		
+		
+		try {
+			cosmosFlightDataUtil.deleteCollection();
+		}
+		catch (DocumentClientException de) {
+			//swallow exception in case the collection is not there.
+		}
+		
+		try {
+			cosmosFlightDataUtil.createCollection();
+		}
+		catch (DocumentClientException de) {
+			//swallow exception in case the collection is not there.
+		}			
+		
+	}
+	
+	@After
+	public void shutdown() throws DocumentClientException, Exception {
+		
+		cosmosFlightDataUtil.deleteCollection();
+				
+	}	
+	
+	@Test
+	public void shouldFindAll() throws Exception {
+			
+		cosmosFlightDataUtil.save(IOUtils.getResourceAsString("data/flightdata/flight1.json", FlightDataConnectivityTest.class));						
+				
+		List<Document> results = cosmosFlightDataUtil.findAll();
+				
+		assertEquals(1, results.size());
+		
+        logger.info("results: {}", results.size());
+        
+	}
+	
+}
